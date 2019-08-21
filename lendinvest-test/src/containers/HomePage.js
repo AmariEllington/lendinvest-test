1840c5740a93d029ede927fa149f597ccdb13749
AmariEllington <amari.l.ellington@gmail.com>
2019-08-21 13:04:13 +0100

    Home Page added and linked to App

---
+++ lendinvest-test/src/containers/HomePage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Data from "./data/current-loans";
+
+export default class HomePage extends Component {
+  state = {};
+  render() {
+    return <div>Hello</div>;
+  }
+}
